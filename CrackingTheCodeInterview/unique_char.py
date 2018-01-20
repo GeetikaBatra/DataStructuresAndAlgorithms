a0442845b53d645c7d5cd0aded31f282227e7a8c
Geetika Batra <geetika791@gmail.com>
2018-01-20 18:02:33 +0530

    Unique using list

--- CrackingTheCodeInterview/unique_char.py
+++ CrackingTheCodeInterview/unique_char.py
@@ -1,6 +1,7 @@
 # assuming characters in teh string are lower case
 # Find if string has unique character
 
+#Bitwise operation
 def find_unique(string1):
     checker = 0
     for each in string1:
@@ -14,3 +15,21 @@ def find_unique(string1):
     return True
 
 print(find_unique("kjhfdkk"))
+
+#using List indices
+def list_implemetation(string2):
+    u_list = []
+    for i in range(0, 128):
+        u_list.append(0)
+    
+    for each in string2:
+        if u_list[ord(each)] > 0:
+            return False
+
+        u_list[ord(each)] = 1
+
+    return True
+
+print(list_implemetation("yes"))
+
+
